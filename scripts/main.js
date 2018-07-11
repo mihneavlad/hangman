999c3e3d2637aded8ae40aa9f551a2824199a053
mihneavlad <mihnel@yahoo.com>
2018-07-11 16:05:40 +0200

    Added event listener to the div container of the category buttons.

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+(function(){
+
+    const baseURL = 'https://od-api.oxforddictionaries.com/api/v1';
+    const proxy = `http://localhost:2337/${baseURL}`;
+    const appKey = 'c8ee174b1558a4e00bab2e6b38df6fee';
+    const appId = '47516d19';
+
+    document.body.querySelector('.button-container').addEventListener('click', event => {
+        event.preventDefault();
+
+        let endpoint = '/wordlist/en/domains=Music';
+
+        $.ajax(proxy + endpoint, {
+            method: 'GET',
+            headers: {
+                'app_key': appKey,
+                'app_id': appId
+            }
+        }).done(response => {
+            document.body.querySelector('#response').innerText = JSON.stringify(response, null, 2);
+        });
+    });
+
+    //
+})();
